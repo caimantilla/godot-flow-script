e2f1fec21b79dd6c4a3e5a7bd7f3c36af867fb74
Caiman <103377610+caimantilla@users.noreply.github.com>
2024-05-01 17:00:38 -0400

    doc_classes

--- config.py
+++ config.py
@@ -7,3 +7,54 @@ def can_build(env, platform):
 
 def configure(env):
 	pass
+
+
+def get_doc_path():
+	return "doc_classes"
+
+
+def get_doc_classes():
+	names = [
+		"FlowConfigManager",
+		"FlowFactory",
+		"FlowTypeDB",
+
+		"FlowScript",
+		"FlowType",
+		"FlowNode",
+		"FlowNodeExecutionState",
+		"FlowFiber",
+		"FlowController",
+		"FlowBridge",
+		"FlowNodeEditor",
+
+		"AssignExpressionToVariableFlowNode",
+		"AssignExpressionToLocalVariableFlowNode",
+		"AssignExpressionToGlobalVariableFlowNode",
+		"CommentFlowNode",
+		"ExecuteExpressionFlowNode",
+		"ExecuteExternalFlowScriptFlowNode",
+		"ExpressionBranchFlowNode",
+		"MultiBranchExecutionFlowNode",
+		"PrintToConsoleFlowNode",
+		"ProcedureFlowNode",
+		"ReturnExpressionFlowNode",
+		"SequentialBranchExecutionFlowNode",
+		"SimultaneousBranchExecutionFlowNode",
+		"WaitSecondsFlowNode",
+		"WhileExpressionTrueLoopFlowNode",
+
+		"AssignExpressionToVariableFlowNodeEditor",
+		"CommentFlowNodeEditor",
+		"ExecuteExpressionFlowNodeEditor",
+		"ExecuteExternalFlowScriptFlowNodeEditor",
+		"ExpressionBranchFlowNodeEditor",
+		"MultiBranchExecutionFlowNodeEditor",
+		"PrintToConsoleFlowNodeEditor",
+		"ProcedureFlowNodeEditor",
+		"ReturnExpressionFlowNodeEditor",
+		"WaitSecondsFlowNodeEditor",
+		"WhileExpressionTrueLoopFlowNodeEditor",
+	]
+
+	return names
